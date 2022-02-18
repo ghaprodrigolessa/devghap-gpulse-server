b2645c2fbafc9e22f5f12485d73908aba340df0a
volbok <rodrigocarvalholessa@gmail.com>
2022-02-18 18:26:38 -0300

    ATUALIZANDO APIRODRIGO

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3333;
 
 app.use(cors());
 app.use(function (req, res, next) {
